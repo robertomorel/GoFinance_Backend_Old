f9d9f13daa7ba287da76ddd9a134ff8230be3928
robertomorel <game.developer.br@gmail.com>
2020-04-15 14:26:40 -0300

    Projeto Finalizado

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,47 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const income = this.transactions
+      .map(t => (t.type === 'income' ? t.value : 0))
+      .reduce((p, n) => p + n, 0);
+    const outcome = this.transactions
+      .map(t => (t.type === 'outcome' ? t.value : 0))
+      .reduce((p, n) => p + n, 0);
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+  }
+
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,31 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, type, value }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && value > total) {
+      throw Error('You dont have enough balance');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
